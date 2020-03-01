69792ccc4b93f7c5a9ccccef14a83861b69be15c
Tomasz Szychliński <s14354@pjwstk.edu.pl>
2020-03-01 17:20:50 +0100

    Pierwszy commit

---
+++ Cw1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace Cw1
+{
+    class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetAsync(args[0]);
+            var body = await response.Content.ReadAsStringAsync();
+            var words = body.Split(' ');
+            foreach(String word in words)
+            {
+                if (word.Contains('@'))
+                    System.Console.Out.WriteLine(word);
+            }
+        }
+    }
+}
